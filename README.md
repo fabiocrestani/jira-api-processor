86ca097da1f64c888ee10eb5d778df88c9d941cd
Fabio Crestani <crestani.fabio@gmail.com>
2020-03-25 22:51:41 +0100

    Reorganize main

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
+# Jira API Queries
+
+## Requirements
 pip install requests
 
+## API Token
+Generate a token here: https://id.atlassian.com/manage/api-tokens
+Convert to base64 and add to the file jira_api_token.py
 
-https://realpython.com/api-integration-in-python/
+## References
+* https://docs.atlassian.com/software/jira/docs/api/REST/1000.824.0/
+
+* https://developer.atlassian.com/cloud/jira/platform/basic-auth-for-rest-apis/
+
+* https://developer.atlassian.com/cloud/jira/platform/rest/v3/?utm_source=%2Fcloud%2Fjira%2Fplatform%2Frest%2F&utm_medium=302#about
+
+* https://realpython.com/api-integration-in-python/
+
+
--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import requests, json
 from jira_api_token import *
 
-headers = {'Authorization' : 'Basic ' + jira_api_token}
+def jiraApiQuery(server, service):
+	headers = {'Authorization' : 'Basic ' + jira_api_token}
+	r = requests.get(server + service, headers=headers, verify=True)
+	return json.loads(r.text)
 
-r = requests.get(
-	'https://sandbox-fc.atlassian.net/rest/api/3/issue/TP-1', 
-	headers=headers, 
-	verify=True)
 
-j = json.loads(r.text)
+def main():
+	server = "https://sandbox-fc.atlassian.net"
 
-print(json.dumps(j, indent=1))
+	# Query one given issue
+	#result = jiraApiQuery(server, "/rest/api/3/issue/TP-1")
+	#print(json.dumps(result, indent=1))
+	
+	# Query list of all issues
+	result = jiraApiQuery(server, "/rest/api/3/issue/picker")
+	print(json.dumps(result, indent=1))
+
+if __name__ == "__main__":
+	# execute only if run as a script
+	main()
